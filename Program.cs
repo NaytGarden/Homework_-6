2b74b4e0c25f7bdad76f4f6b114e5066f5ca1ddd
NaytGarden <Nayt2196@yandex.ru>
2023-03-20 00:37:20 +0300

    Complete

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+int Num()// функция для определения элемента массива
+{
+    int N = new Random().Next(-100,101);
+    return N;
+}
+Console.WriteLine("Введите размер массива");
+int Numb = Convert.ToInt32(Console.ReadLine());
+int[] M;
+M = new int[Numb];
+int count = 0;
+for (int i = 0; i < M.Length; i++) {M[i] = Num();System.Console.WriteLine();}
+for (int i = 0; i < M.Length; i++)
+{
+    if(M[i] > 0){count++; Console.Write(count);}
+    
+}
+*/
+
+
+ 
+
+
+
